b4d82df98b3f508f3124f59c83c94f11349fd4fc
rbangamm <ravindu.angammana@gmail.com>
2018-01-20 22:41:39 -0500

    Started adding multi-threading capabilities

--- main.py
+++ main.py
@@ -1,58 +1,29 @@
-from picamera.array import PiRGBArray
-from picamera import PiCamera
+import Queue
 import time
-import cv2
-camera = PiCamera()
+import threading
+from video_capture import run_video
 
-camera.resolution = (800, 600)
-camera.framerate = 90
-rawCapture = PiRGBArray(camera, size=(800, 600))
-
-#Prepare camera
-time.sleep(0.2)
-#do setup
-i = 0
-dx = 0
-currentPos = 0
-prevPos = 0
-criticalPos = 36
-averageFaceWidth = 6.5
-#0 is travel straight, 1 is avoid
-state = 0
-
-for frame in camera.capture_continuous(rawCapture, format="bgr", use_video_port = True):
-	image = frame.array
-
-	faceCascade = cv2.CascadeClassifier("classifiers/haarcascade_frontalface_default.xml")
-
-	key = cv2.waitKey(1) & 0xFF
-
-	gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-	#Apply classifier
-	faces = faceCascade.detectMultiScale(
-    		gray,
-    		scaleFactor=1.2,
-    		minNeighbors=5,
-    		minSize=(30, 30),
-    		flags = cv2.cv.CV_HAAR_SCALE_IMAGE
-	)
-	#Draw bounding boxes
-	if (len(faces) != 0):
-		x, y, w, h = faces[0]
-		cv2.rectangle(image, (x,y), (x+w, y+h), (0, 255, 0), 2)
-		currentPos = (2714 * averageFaceWidth)/w
-		dx = currentPos - prevPos
-		if (currentPos <= criticalPos):
-			state = 1;
-		prevPos = currentPos
-
-	if (state == 1):
-		#
+class thisThread(threading.Thread):
+    def __init__(self, threadID):
+	threading.Thread.__init__(self)
+	self.threadID = threadID
+    def run(self):
+	#A value of 1 runs the video thread
+	if (threadID):
+	    run_video()
+	#A value of 0 runs the signal thread
 	else:
-		#
+	    pass
+	    #Your code goes here and should replace the pass
 
+vid_thread = thisThread(1)
+sig_thread = thisThread(0)
 
+q = Queue()
 
-	i+=1
+vid_thread.start()
+sig_thread.start()
 
-	rawCapture.truncate(0)
+if (not q.empty()):
+    pass
+    #Insert code here to replace the pass
